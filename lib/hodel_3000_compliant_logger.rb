7e1e89c54083f74326d05e6c4efad1a91dd3edab
jasonhutchens <jason@agworld.com.au>
2013-06-20 10:34:48 +0800

    gemify

--- lib/hodel_3000_compliant_logger.rb
+++ lib/hodel_3000_compliant_logger.rb
@@ -1,10 +1,8 @@
 require 'logger'
 require 'English'
-# Jan  2 03:38:05 topfunky postfix/postqueue[2947]: warning blah blah blah
+require 'socket'
 
-##
 # A logger for use with pl_analyze and other tools that expect syslog-style log output.
-
 class Hodel3000CompliantLogger < Logger
 
   ##
@@ -13,12 +11,6 @@ class Hodel3000CompliantLogger < Logger
   def format_message(severity, timestamp, progname, msg)
     "#{timestamp.strftime("%b %d %H:%M:%S")} #{hostname} rails[#{process_id}]: #{msg2str(msg).gsub(/\n/, '').lstrip}\n"
   end
-
-  # original method, pre-patch for Exception handling:
-  #
-  # def format_message(severity, timestamp, msg, progname)
-  #   "#{timestamp.strftime("%b %d %H:%M:%S")} #{Socket.gethostname.split('.').first} rails[#{$PID}]: #{progname.gsub(/\n/, '').lstrip}\n"
-  # end
 
   private
 
--- spec/hodel_3000_compliant_logger_spec.rb
+++ spec/hodel_3000_compliant_logger_spec.rb
@@ -1,38 +1,45 @@
 $: << File.dirname(__FILE__) + "/../lib"
 
-require 'rubygems'
-require 'spec'
+require 'rspec'
 require 'hodel_3000_compliant_logger'
+require 'stringio'
 
-describe Hodel3000CompliantLogger do 
+describe Hodel3000CompliantLogger do
 
-	before :each do
-		@out = StringIO.new
-		@log = Hodel3000CompliantLogger.new(@out)
-	end
+  before :each do
+    @out = StringIO.new
+    @log = Hodel3000CompliantLogger.new(@out)
+    Socket.stub(:gethostname).and_return('hostname.domain')
+  end
 
-	it "should log stuff in a syslog-like format so that Eric Hodel's Rails Analyzer Tools can parse it" do
-		msg = "Yo ho hello there!"
-		@log.info(msg)
-		@out.string.should match(/^\w{3} \d{2} \d{2}:\d{2}:\d{2} \w+ rails\[\d+\]: #{msg}\n$/)
-	end
-	
-	it "should handle an Exception object used as an argument in Logger#error, rather than blow chunks" do
-		@log.error(Exception.new)
-		@out.string.should match(/Exception/)
-	end
+  it "should log stuff in a syslog-like format so that Eric Hodel's Rails Analyzer Tools can parse it" do
+    msg = "Yo ho hello there!"
+    @log.info(msg)
+    @out.string.should match(/^\w{3} \d{2} \d{2}:\d{2}:\d{2} hostname rails\[\d+|\d+\]: #{msg}\n$/)
+  end
 
-	it "should display a semi-readable stack trace (albiet on one line) when Logger#error(SomeException) was called" do
-		@log.error(toss_runtime_error)
-		@out.string.should match(/.*? \| .*? \| .*? \|/) # pipe separated stack frames
-		@out.string.should match(/\n$/)
-		@out.string.count("\n").should == 1 
-	end
-	
-	def toss_runtime_error
-		raise "Catastrophic Failure"
-	rescue => e
-		return e
-	end
+  it "should handle an Exception object used as an argument in Logger#error, rather than blow chunks" do
+    @log.error(Exception.new)
+    @out.string.should match(/Exception/)
+  end
+
+  it "should display a semi-readable stack trace (albiet on one line) when Logger#error(SomeException) was called" do
+    @log.error(toss_runtime_error)
+    @out.string.should match(/.*? \| .*? \| .*? \|/) # pipe separated stack frames
+    @out.string.should match(/\n$/)
+    @out.string.count("\n").should == 1
+  end
+
+  it "should replace newlines in message with an empty string" do
+    msg = "Yo\nho\nhello\nthere!"
+    @log.info(msg)
+    @out.string.should match(/^\w{3} \d{2} \d{2}:\d{2}:\d{2} hostname rails\[\d+:\d+\]: Yohohellothere!\n$/)
+  end
+
+  def toss_runtime_error
+    raise "Catastrophic Failure"
+  rescue => e
+    return e
+  end
 end
-	 
+
